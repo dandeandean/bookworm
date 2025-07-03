614e3125210418b510f7e942a189b72f4313cbae
DanDeanDean <dpd5518@psu.edu>
2025-07-03 10:35:47 -0400

    complete

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -20,9 +20,9 @@ func getNamesThenTagsCmp(cmd *cobra.Command, args []string, toComplete string) (
 func getTagsCmp(_ *cobra.Command, args []string, _ string) ([]cobra.Completion, cobra.ShellCompDirective) {
 	out := make([]string, len(Bw.Cfg.BookMarks))
 	for _, b := range Bw.Cfg.BookMarks {
-		for _, t := range b.Tags {
-			if !slices.Contains(args, t) {
-				out = append(out, t)
+		for _, tag := range b.Tags {
+			if !slices.Contains(args, tag) {
+				out = append(out, tag)
 			}
 		}
 	}
@@ -31,11 +31,11 @@ func getTagsCmp(_ *cobra.Command, args []string, _ string) ([]cobra.Completion, 
 
 func getNamesCmp(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
 	if len(args) > 0 {
-		return []string{}, cobra.ShellCompDirectiveNoFileComp
+		return nonCmp(cmd, args, toComplete)
 	}
 	out := make([]string, len(Bw.Cfg.BookMarks))
-	for _, b := range Bw.Cfg.BookMarks {
-		out = append(out, b.Name)
+	for k := range Bw.Cfg.BookMarks {
+		out = append(out, k)
 	}
 	return out, cobra.ShellCompDirectiveNoFileComp
 }
