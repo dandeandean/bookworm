536941f4437893b8754e02cedbefe0325345435c
DanDeanDean <dpd5518@psu.edu>
2025-07-03 10:27:00 -0400

    add tagging completion

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -1,12 +1,38 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"slices"
+
+	"github.com/spf13/cobra"
+)
 
 func nonCmp(_ *cobra.Command, _ []string, _ string) ([]cobra.Completion, cobra.ShellCompDirective) {
 	return []string{}, cobra.ShellCompDirectiveNoFileComp
 }
 
+func getNamesThenTagsCmp(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
+	if len(args) == 0 {
+		return getNamesCmp(cmd, args, toComplete)
+	}
+	return getTagsCmp(cmd, args, toComplete)
+}
+
+func getTagsCmp(_ *cobra.Command, args []string, _ string) ([]cobra.Completion, cobra.ShellCompDirective) {
+	out := make([]string, len(Bw.Cfg.BookMarks))
+	for _, b := range Bw.Cfg.BookMarks {
+		for _, t := range b.Tags {
+			if !slices.Contains(args, t) {
+				out = append(out, t)
+			}
+		}
+	}
+	return out, cobra.ShellCompDirectiveNoFileComp
+}
+
 func getNamesCmp(cmd *cobra.Command, args []string, toComplete string) ([]cobra.Completion, cobra.ShellCompDirective) {
+	if len(args) > 0 {
+		return []string{}, cobra.ShellCompDirectiveNoFileComp
+	}
 	out := make([]string, len(Bw.Cfg.BookMarks))
 	for _, b := range Bw.Cfg.BookMarks {
 		out = append(out, b.Name)
--- cmd/root.go
+++ cmd/root.go
@@ -89,7 +89,7 @@ var tagCmd = &cobra.Command{
 	Use:               "tag name tags...",
 	Args:              cobra.MinimumNArgs(2),
 	Short:             "Tag boomarks with tags.",
-	ValidArgsFunction: getNamesCmp,
+	ValidArgsFunction: getNamesThenTagsCmp,
 	Run: func(cmd *cobra.Command, args []string) {
 		err := Bw.SetTags(args[0], args[1:])
 		if err != nil {
