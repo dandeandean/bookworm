543e2edee58a32fc1820d41e37c9e021cd641fa9
DanDeanDean <dpd5518@psu.edu>
2025-09-03 20:36:03 -0400

    add fixes to dbwrite

--- internal/state.go
+++ internal/state.go
@@ -2,47 +2,53 @@ package internal
 
 import (
 	"encoding/json"
+	"time"
+
 	"go.etcd.io/bbolt"
 )
 
-func (c *Config) WriteBookMark(bm BookMark) error {
-	db, err := bbolt.Open(c.DbPath, 0600, nil)
+func (c Config) WriteBookMark(bm BookMark) error {
+	db, err := bbolt.Open(c.DbPath, 0600, &bbolt.Options{Timeout: time.Second})
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	buf, err := json.Marshal(bm)
 	if err != nil {
 		panic(err)
 	}
 	err = db.Update(func(tx *bbolt.Tx) error {
-		bookMarksBucket, err := tx.CreateBucketIfNotExists([]byte("BookMarks"))
+		bookMarksBucket, err := tx.CreateBucketIfNotExists([]byte("bookmarks"))
 		if err != nil {
-			return err
+			panic(err)
 		}
-		buf, err := json.Marshal(bm)
-		if err != nil {
-			return err
-		}
-		bookMarksBucket.Put([]byte(bm.Name), buf)
-		return nil
+		return bookMarksBucket.Put([]byte(bm.Name), buf)
 	})
 	return nil
 }
 
-func (c *Config) GetBookMark(key string) (*BookMark, error) {
-	db, err := bbolt.Open(c.DbPath, 0600, &bbolt.Options{ReadOnly: true})
+func (c Config) GetBookMark(key string) (*BookMark, error) {
+	db, err := bbolt.Open(c.DbPath, 0600, &bbolt.Options{ReadOnly: true, Timeout: time.Second})
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 	bmToReturn := &BookMark{}
-	err = db.View(func(tx *bbolt.Tx) error {
-		bookMarksBucket := tx.Bucket([]byte("BookMarks"))
-		if bookMarksBucket == nil {
-			return err
-		}
-		buf := bookMarksBucket.Get([]byte(key))
-		err := json.Unmarshal(buf, bmToReturn)
-		if err != nil {
-			return err
-		}
-		return nil
-	})
+	var buf []byte
+	err = db.View(
+		func(tx *bbolt.Tx) error {
+			bookMarksBucket := tx.Bucket([]byte("bookmarks"))
+			if bookMarksBucket == nil {
+				return err
+			}
+			buf = bookMarksBucket.Get([]byte(key))
+			return nil
+		})
+
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(buf, bmToReturn)
 	if err != nil {
 		return nil, err
 	}
