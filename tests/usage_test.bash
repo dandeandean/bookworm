1c83543bcec5eaa2abb1220244547254b9e0eeec
DDD <58938832+dandeandean@users.noreply.github.com>
2025-09-21 20:12:31 -0400

    Integration Tests (#14)
    * fix file perms: now init works on second try
    
    * Correct Empty Config Directory Handling
    
    * add test for install
    
    * workflow dispatch
    
    * undo that
    
    * add test to ci
    
    * most comprehensive tests I can think of
    
    * fix path
    
    * fix name of file
    
    * fix again
    
    * Fix errors I found while testing
    
    * Clean

---
+++ tests/usage_test.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "Testing install"
+
+make install
+
+if ! command -v bookworm 2>/dev/null ; then
+	echo "failed to install"
+	exit 1
+fi
+
+echo "===================="
+
+echo "Testing pre-init"
+
+bookworm >/dev/null
+
+if [[ $? != 2 ]]; then
+	exit 1
+fi
+
+echo "===================="
+
+echo "Testing init"
+
+if ! bookworm init >/dev/null; then
+	echo "failed to init"
+	exit 1
+fi
+
+echo "===================="
+echo "Testing create new bookmark"
+
+if bookworm make google foo.bar >/dev/null; then
+	echo "failed to catch invalid bookmark"
+	exit 1
+fi
+
+if ! bookworm make google https://www.google.com >/dev/null; then
+	echo "failed to create bookmark"
+	exit 1
+fi
+
+if ! bookworm ls | grep -q "google"; then
+	echo "Couldn't find google in the bookmarks"
+	exit 1
+fi
+
+echo "Passed!"
