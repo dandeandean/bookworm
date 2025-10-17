afd3bb96a8223cd7f923c19e212592eac8b155cb
DanDeanDean <dpd5518@psu.edu>
2025-10-17 11:28:35 -0400

    add release pipeline step

--- build/bookworm.rb
+++
@@ -1,7 +0,0 @@
-class Bookworm < Formula
-  desc "Browser bookmarks, but better"
-  homepage "https://github.com/dandeandean/bookworm"
-  license "MIT"
-  head "https://github.com/dandeandean/bookworm.git", branch: "main"
-
-end
