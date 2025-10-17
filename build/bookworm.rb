951a8f76e290b69c9e50cb262072cd96085b265e
DanDeanDean <dpd5518@psu.edu>
2025-10-17 10:10:11 -0400

    add some ruby

---
+++ build/bookworm.rb
@@ -0,0 +1,7 @@
+class Bookworm < Formula
+  desc "Browser bookmarks, but better"
+  homepage "https://github.com/dandeandean/bookworm"
+  license "MIT"
+  head "https://github.com/dandeandean/bookworm.git", branch: "main"
+
+end
